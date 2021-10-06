2d58bafc0cc1fb0fa6c91887f0d2deb13ff334f6
pauleman <paul@fiskaaen.com>
2021-10-06 10:12:13 +0200

    First push

---
+++ Paul02App/Paul02App/Paul02AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Paul02AppApp.swift
+//  Paul02App
+//
+//  Created by paulemann on 06/10/2021.
+//
+
+import SwiftUI
+
+@main
+struct Paul02AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
